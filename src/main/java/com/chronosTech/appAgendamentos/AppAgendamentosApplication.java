aebe0cb0b92beab4765483a22e152ab18a2e49d7
Adrian <adrianpardalcaldeiras@gmail.com>
2025-08-14 23:21:39 -0300

    Começo da Aplicação

---
+++ src/main/java/com/chronosTech/appAgendamentos/AppAgendamentosApplication.java
@@ -0,0 +1,13 @@
+package com.chronosTech.appAgendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class AppAgendamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAgendamentosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chronosTech/appAgendamentos/AppAgendamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronosTech.appAgendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAgendamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
